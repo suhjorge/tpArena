3c352f4052c4c343128f96cb7a1fdcc4a9d5131e
jorge <suhjorge@gmail.com>
2018-07-13 12:22:37 -0300

    se agregan mas cosas a la segunda ventana

--- src/main/java/ui/windows/DatosDelAlumnoWindow.java
+++ src/main/java/ui/windows/DatosDelAlumnoWindow.java
@@ -16,6 +16,27 @@ public class DatosDelAlumnoWindow extends Dialog<DatosDelAlumnoViewModel> {
 	public DatosDelAlumnoWindow(WindowOwner owner) {
 		super(owner, new DatosDelAlumnoViewModel());
 	}
+	
+	@Override
+	public void createContents(Panel mainPanel) {
+		Panel form = new Panel(mainPanel);
+		form.setLayout(new ColumnLayout(2));
+		
+		new Label(form).setText("Nombre:");
+		new Label(form).setText("Osqui");
+		
+		new Label(form).setText("Apellido:");
+		new Label(form).setText("Cubas");
+		
+		new Label(form).setText("Cuenta Git:");
+		new Label(form).setText("of.cubas");
+		
+		new Label(form).setText("Legajo:");
+		new Label(form).setText("1413478");
+		
+		new Button(form).setCaption("Notas");
+		new Button(form).setCaption("Modificar datos");
+	}
 
 	@Override
 	protected void createFormPanel(Panel mainPanel) {
